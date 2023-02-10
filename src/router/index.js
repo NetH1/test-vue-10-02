53ce3f9181f7f616ef2c63f1e42dfc2777c4494a
NetH1 <youtube_17@mail.ru>
2023-02-10 08:17:39 +0600

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SinglePage from '../components/SinglePage.vue'
+// import LocalFetch from '../views/LocalFetch.vue'
+import SinglePageAPI from '../components/SinglePageAPI.vue'
+import FavoritesList from '../views/FavoritesList.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/single/:id',
+    name: 'single',
+    component: SinglePage
+  },
+  {
+    path: '/singleAPI/:id',
+    name: 'singleAPI',
+    component: SinglePageAPI
+  },
+  {
+    path: '/favorites',
+    name: 'favorites',
+    component: FavoritesList
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
